644781fd2ac25330019b46f38c9e38219188f82e
ethere <3318216036@qq.com>
2020-11-22 20:37:21 +0800

    创建和打印扑克牌

--- src/index.ts
+++ src/index.ts
@@ -1,82 +1,46 @@
-// function add(a:number,b:number):number{
-//     return a + b
-// }
-
-import { type } from "os";
-
-// const result = add(1,2);
-
-// let name = '123';
-
-// // let arr:Array<number>;
-// let arr:number[];
-// arr = [1,2]
-
-//联合类型
-let str : string | undefined;
-if(typeof str === 'string'){//类型保护
-    str = str.concat('end')
+type Color = "♥"|"♠"|"♣"|"♦";
+type Poker = {
+    color:Color,
+    mark:number
 }
+type Pokers = Poker[];
 
-//void
-function print(){
-    console.log('print')
-}
-
-//never
-function throwErr():never{
-    throw new Error('这是一个错误')
-}
-function test():never{
-    while(true) console.log('死循环');
-}
-
-//字面量类型
-let sex : '男'|'女';
-let arr : [];
-let user : {
-    name:string,
-    age:number
-}
-
-//元组类型
-let values:[string,number];
-
-//any类型
-let any : any;
-values = any;//对any类型不进行检查
-
-//类型别名
-type Gender = '男'|'女'
-type User = {
-    name:string,
-    age:number,
-    gender:Gender
-}
-function users(g:Gender) :User[]{
-    return []
-}
-
-//函数的相关约束
-function conbine(a:number,b:number):number;
-function conbine(a:string,b:string):string;
-function conbine(a:number|string,b:number|string):number|string{
-    if(typeof a === 'number' && typeof b === 'number'){
-        return a * b
-    }else if(typeof a === 'string' && typeof b === 'string'){
-        return a + b
+function createPoker():Pokers{
+    const pokers:Pokers = [];
+    for (let i = 0; i < 13; i++) {
+        pokers.push({
+            color:"♥",
+            mark:i+1
+        },{
+            color:"♠",
+            mark:i+1
+        },{
+            color:"♣",
+            mark:i+1
+        },{
+            color:"♦",
+            mark:i+1
+        })
     }
-    throw new Error('a和b必须是同类型')
+    return pokers;
 }
-const result = conbine(1,1)
-
-function add(a:number,b:number,c?:number){
-    if(c){
-        return a + b + c
-    }
-    return a + b
+const pokers = createPoker()
+function printPoker(){
+    let result = '\n'
+    pokers.forEach((poker,i)=>{
+        let str = poker.color;
+        if(poker.mark <= 10){
+            str += poker.mark;
+        }else if(poker.mark === 11){
+            str += 'J'
+        }else if(poker.mark === 12){
+            str += 'Q'
+        }else if(poker.mark === 13){
+            str += 'K'
+        };
+        result += str + '\t';
+        if((i+1)%6 === 0) result += '\n'
+    })
+    console.log(result)
 }
-function printA(a=1){
-    console.log(a)
-}
-printA(1)
+printPoker()
