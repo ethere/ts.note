0d06d6aa072f0ea8f92c7df879cad8ffb656489e
ethere <3318216036@qq.com>
2020-11-25 23:58:12 +0800

    enum

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,41 @@
-type Color = "♥"|"♠"|"♣"|"♦";
+enum Color{
+    heart = "♥",
+    spade = "♠",
+    club = "♣",
+    diamond = "♦"
+}
+enum Mark{
+    A = "A",
+    two = "2",
+    three = "3",
+    four = "4",
+    five = "5",
+    six = "6",
+    seven = "7",
+    eight = "8",
+    nine = "9",
+    ten = "10",
+    eleven = "J",
+    twelve = "Q",
+    king = "K"
+}
 type Poker = {
     color:Color,
-    mark:number
+    mark:Mark
 }
 type Pokers = Poker[];
 
 function createPoker():Pokers{
     const pokers:Pokers = [];
-    for (let i = 0; i < 13; i++) {
-        pokers.push({
-            color:"♥",
-            mark:i+1
-        },{
-            color:"♠",
-            mark:i+1
-        },{
-            color:"♣",
-            mark:i+1
-        },{
-            color:"♦",
-            mark:i+1
-        })
+    const marks = Object.values(Mark);
+    const colors = Object.values(Color);
+    for (const mark of marks) {
+        for (const color of colors) {
+            pokers.push({
+                color,
+                mark
+            })
+        }
     }
     return pokers;
 }
@@ -28,16 +43,7 @@ const pokers = createPoker()
 function printPoker(){
     let result = '\n'
     pokers.forEach((poker,i)=>{
-        let str = poker.color;
-        if(poker.mark <= 10){
-            str += poker.mark;
-        }else if(poker.mark === 11){
-            str += 'J'
-        }else if(poker.mark === 12){
-            str += 'Q'
-        }else if(poker.mark === 13){
-            str += 'K'
-        };
+        let str = poker.color + poker.mark;
         result += str + '\t';
         if((i+1)%6 === 0) result += '\n'
     })
