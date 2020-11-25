98388972a5407ed6b7e52ea0a21a73633aaeeae4
ethere <3318216036@qq.com>
2020-11-25 23:48:58 +0800

    enum

--- src/index.ts
+++ src/index.ts
@@ -1,46 +1,8 @@
-type Color = "♥"|"♠"|"♣"|"♦";
-type Poker = {
-    color:Color,
-    mark:number
+enum level{
+    level1 = 1,
+    level2 = 2,
+    level3 = 3,
+    level4
 }
-type Pokers = Poker[];
-
-function createPoker():Pokers{
-    const pokers:Pokers = [];
-    for (let i = 0; i < 13; i++) {
-        pokers.push({
-            color:"♥",
-            mark:i+1
-        },{
-            color:"♠",
-            mark:i+1
-        },{
-            color:"♣",
-            mark:i+1
-        },{
-            color:"♦",
-            mark:i+1
-        })
-    }
-    return pokers;
-}
-const pokers = createPoker()
-function printPoker(){
-    let result = '\n'
-    pokers.forEach((poker,i)=>{
-        let str = poker.color;
-        if(poker.mark <= 10){
-            str += poker.mark;
-        }else if(poker.mark === 11){
-            str += 'J'
-        }else if(poker.mark === 12){
-            str += 'Q'
-        }else if(poker.mark === 13){
-            str += 'K'
-        };
-        result += str + '\t';
-        if((i+1)%6 === 0) result += '\n'
-    })
-    console.log(result)
-}
-printPoker()
+const now = level.level4;
+console.log(now)
