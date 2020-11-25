5a66a042c10718abe04190e9cf193a54d8030e2a
ethere <3318216036@qq.com>
2020-11-26 00:20:09 +0800

    位枚举

---
+++ src/index copy 2.ts
@@ -0,0 +1,8 @@
+enum level{
+    level1 = 1,
+    level2 = 2,
+    level3 = 3,
+    level4
+}
+const now = level.level4;
+console.log(now)
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,21 @@
-enum level{
-    level1 = 1,
-    level2 = 2,
-    level3 = 3,
-    level4
+enum Permisson{
+    Read = 1, //0001
+    Write = 2, //0010
+    Create = 4, //0100
+    Delete = 8  //1000
 }
-const now = level.level4;
-console.log(now)
+
+//权限组合，或运算
+const p = Permisson.Read | Permisson.Write;
+console.log(p)
+
+//判断是否拥有某个权限，与运算
+function hasPermisson(target:Permisson,per:Permisson){
+    return (target & per) === per
+}
+console.log(hasPermisson(p,Permisson.Read))
+
+//删除某个权限，异或运算
+const newPer = p ^ Permisson.Read;
+console.log(newPer);
+console.log(hasPermisson(newPer,Permisson.Read))
